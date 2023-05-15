3527fccf3fa8a10d24561c2ad00d7eb639a3ef10
Agustín Zabalgoitia <99134268+Agustin-Zabalgoitia@users.noreply.github.com>
2023-05-15 11:41:02 -0300

    Add files via upload

---
+++ colecciones/test/colecciones/mapaTest.java
@@ -0,0 +1,15 @@
+package colecciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mapaTest {
+
+	@Test
+	public void test() {
+		mapa m = new mapa();
+		m.ejer();
+	}
+
+}
